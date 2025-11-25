0c33bcdb30b23d74bde244665c90a3203892b5d7
Mian <60836125+MianReplicate@users.noreply.github.com>
2025-11-24 22:54:46 -0800

    bruh the chaser class didnt add

---
+++ Source/GenderAcceptance/Mian/ThoughtWorkers/Need_Chaser.cs
@@ -0,0 +1,30 @@
+﻿using GenderAcceptance.Mian.Needs;
+using RimWorld;
+using Verse;
+
+namespace GenderAcceptance.Mian.ThoughtWorkers;
+
+public class Need_Chaser : ThoughtWorker
+{
+    protected override ThoughtState CurrentStateInternal(Pawn p)
+    {
+        var chaserNeed = (Needs.Need_Chaser)p.needs.TryGetNeed(GADefOf.Need_Chaser);
+        if (chaserNeed == null)
+            return ThoughtState.Inactive;
+        switch (chaserNeed.CurCategory)
+        {
+            case ChaserCategory.JustHadIntimacy:
+                return ThoughtState.ActiveAtStage(0);
+            case ChaserCategory.Neutral:
+                return ThoughtState.Inactive;
+            case ChaserCategory.LongWhile:
+                return ThoughtState.ActiveAtStage(1);
+            case ChaserCategory.ExtremelyLongWhile:
+                return ThoughtState.ActiveAtStage(2);
+            case ChaserCategory.Aching:
+                return ThoughtState.ActiveAtStage(3);
+            default:
+                return ThoughtState.Inactive;
+        }
+    }
+}
